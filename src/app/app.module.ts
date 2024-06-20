42c571359f65ac820b10170aa7dda7421f4b3a3d
WithinUA <withinua@gmail.com>
2024-06-20 13:49:39 +0300

    new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ButtonsComponent } from './components/buttons/buttons.component';
+import { AutoComponent } from './components/auto/auto.component';
+import { PlaneComponent } from './components/plane/plane.component';
+import { ShipComponent } from './components/ship/ship.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonsComponent,
+    AutoComponent,
+    PlaneComponent,
+    ShipComponent
   ],
   imports: [
     BrowserModule
